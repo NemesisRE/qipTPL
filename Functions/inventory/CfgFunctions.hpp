f95649096c6119908597f2996c0d08a9ad139f01
NemsisRE <nemesissre@gmail.com>
2016-12-12 04:40:01 +0100

    Much easier instead of changing the whol code every time, use a an action

--- Functions/inventory/CfgFunctions.hpp
+++ Functions/inventory/CfgFunctions.hpp
@@ -1,22 +1,5 @@
 class inventory
 {
 	file = "\qipTPL\Functions\inventory";
-	class addVirtualBackpackCargo {};
-	class addVirtualItemCargo {};
-	class addVirtualMagazineCargo {};
-	class addVirtualWeaponCargo {};
-	class arsenal {};
-	class baseWeapon {};
-	class compatibleItems {};
-	class deleteInventory {};
 	class exportInventory {};
-	class getVirtualBackpackCargo {};
-	class getVirtualItemCargo {};
-	class getVirtualMagazineCargo {};
-	class getVirtualWeaponCargo {};
-	class removeVirtualBackpackCargo {};
-	class removeVirtualItemCargo {};
-	class removeVirtualMagazineCargo {};
-	class removeVirtualWeaponCargo {};
-	class saveInventory {};
 };
